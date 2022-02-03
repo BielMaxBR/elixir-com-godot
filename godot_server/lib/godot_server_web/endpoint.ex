5e2bb8fd4d45efffd5cf9bbd88e9b1d2e0b5d1fd
Gabriel Maximo <gbielmaximo@gmail.com>
2022-02-02 21:08:13 -0300

    man, o js recebeu!

--- godot_server/lib/godot_server_web/endpoint.ex
+++ godot_server/lib/godot_server_web/endpoint.ex
@@ -12,6 +12,7 @@ defmodule GodotServerWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
+  socket "/socket", GodotServerWeb.UserSocket, websocket: true, longpoll: false
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
